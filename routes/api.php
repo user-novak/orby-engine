0758deb07f2f20723fb08ba9f502c45778463b59
novak dev <novakmzv@gmail.com>
2026-02-28 00:06:51 -0500

    feat: se añadio ruta de cuentas

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\ClientController;
+use App\Http\Controllers\Api\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('clients', ClientController::class);
 Route::post('clients/bulk', [ClientController::class, 'bulkStore']);
+
+Route::apiResource('accounts', AccountController::class);
+Route::post('accounts/bulk', [AccountController::class, 'bulkStore']);
